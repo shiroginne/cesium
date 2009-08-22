25b5b4273f677538e1e6b8f4918dc9d47e15c7a6
pyromaniac <pyromaniac@guard.(none)>
2009-08-22 23:08:09 +0400

    Application controller works fine now

--- init.rb
+++ init.rb
@@ -1,7 +1,2 @@
-# Include hook code here
 require "authlogic"
-
-require File.dirname(__FILE__) + "/lib/extentions"
-require File.dirname(__FILE__) + "/lib/form_builder"
-require File.dirname(__FILE__) + "/lib/object"
-require File.dirname(__FILE__) + "/lib/application_controller"
+require File.dirname(__FILE__) + "/lib/cesium"
--- lib/cesium.rb
+++ lib/cesium.rb
@@ -1,4 +1,8 @@
-# Cesium
-module Cesium
-  
+require File.dirname(__FILE__) + "/cesium/application_controller"
+require File.dirname(__FILE__) + "/extentions"
+require File.dirname(__FILE__) + "/form_builder"
+require File.dirname(__FILE__) + "/object"
+
+ActionController::Base.class_eval do
+  include Cesium::ApplicationController
 end
