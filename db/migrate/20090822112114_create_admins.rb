7aefe36f8010c0b05e82873304ee886f339d4aef
pyromaniac <kinwizard@gmail.com>
2009-09-27 23:54:10 +0400

    Added specs. Fixed migrations.

--- db/migrate/20090822112114_create_admins.rb
+++ db/migrate/20090822112114_create_admins.rb
@@ -1,6 +1,5 @@
 class CreateAdmins < ActiveRecord::Migration
   def self.up
-    drop_table :users
     create_table :admins do |t|
       t.string :login, :null => false
       t.string :email, :null => false
@@ -14,14 +13,5 @@ class CreateAdmins < ActiveRecord::Migration
 
   def self.down
     drop_table :admins
-    create_table :users do |t|
-      t.string :login, :null => false
-      t.string :email, :null => false
-      t.string :crypted_password, :null => false
-      t.string :password_salt, :null => false
-      t.string :persistence_token, :null => false
-
-      t.timestamps
-    end
   end
 end
