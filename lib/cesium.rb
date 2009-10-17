c5cbd07facca702e34af09be5c9cb5d98d0e6e2f
pyromaniac <kinwizard@gmail.com>
2009-10-18 00:58:55 +0400

    Fixed readme file

--- lib/cesium.rb
+++ lib/cesium.rb
@@ -2,7 +2,6 @@ require File.dirname(__FILE__) + "/extentions"
 require File.dirname(__FILE__) + "/form_builder"
 
 module Cesium
-
 end
 
 ActionController::Dispatcher.middleware.use Cesium::Rack::StaticOverlay, File.join(File.dirname(__FILE__), '../public')
