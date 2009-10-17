2316c2f3e6335e0fb5ccfcbc4f372862ea145f52
pyromaniac <kinwizard@gmail.com>
2009-10-17 19:29:39 +0400

    Now we again need to add routes for cesium in application routes.rb

--- config/last_route.rb
+++
@@ -1,3 +0,0 @@
-ActionController::Routing::Routes.draw do |map|
-  map.cesium '*url', :controller => 'pages', :action => 'show'
-end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,3 @@ ActionController::Routing::Routes.draw do |map|
   end
 
 end
-
-ActionController::Routing::Routes.add_configuration_file(File.join(File.dirname(__FILE__), 'last_route.rb'))
